f9128e9211e4322032eb57c24f7446ff1a05b184
Artyom Pertsovsky <artyom@pertsovsky.com>
2016-02-06 00:55:07 +0200

    Add gif model

---
+++ app/models/gif.rb
@@ -0,0 +1,14 @@
+class Gif < ActiveRecord::Base
+  validates :uid, uniqueness: true
+
+  def tags=(tags)
+    if tags.kind_of? Array
+      super(tags)
+    else
+      tags                = tags
+      tags_without_spaces = tags.gsub(/\s+/, '')
+      tags_array          = tags_without_spaces.split(',')
+      super(tags_array)
+    end
+  end
+end
