2ad92b7fc86822c8e09f019245e79373ef8cc03b
Artyom Pertsovsky <artyom@pertsovsky.com>
2016-02-06 00:59:20 +0200

    Add gifs search spec

---
+++ spec/services/giphy/search_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Giphy::Search do
+  describe '#by_title', :vcr do
+    let(:query) { 'lol' }
+    let(:giphy_search) { Giphy::Search.new }
+
+    context 'given correct arguments' do
+      it 'returns valid body with content' do
+        gifs = giphy_search.by_title(query)
+        expect(gifs).not_to be_nil
+      end
+    end
+
+    context 'given incorrect arguments type' do
+      it 'should raise InvalidSearchArgs exception' do
+        expect { giphy_search.by_title([1, 2, 3]) }.to raise_error(InvalidSearchArgs)
+      end
+    end
+  end
+end
